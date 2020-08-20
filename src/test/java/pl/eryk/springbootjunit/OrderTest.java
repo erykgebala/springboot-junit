905540df50637a26791d89f0a35cf4f1b41e58f3
erykgebala <erykgebala@gmail.com>
2020-08-20 22:00:43 +0200

    Order tests

--- src/test/java/pl/eryk/springbootjunit/OrderTest.java
+++ src/test/java/pl/eryk/springbootjunit/OrderTest.java
@@ -2,6 +2,10 @@ package pl.eryk.springbootjunit;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderTest {
@@ -14,7 +18,41 @@ class OrderTest {
         assertArrayEquals(ints1, ints2);
     }
 
-    public void addedMealShouldBeInOrderMeals() {
+    @Test
+    public void orderMealsShouldNotBeEmptyAfterAddingMealToOrder() {
+        //given
+        Order order = new Order();
+        Meal meal = new Meal("Pizza", 34);
+
+        //when
+        order.addMealToOrder(meal);
+
+        //then
+        assertEquals(order.getMeals().size(), 1);
+        assertThat(order.getMeals(), not(empty()));             //hamcrest
+        assertThat(order.getMeals().size(), equalTo(1));//hamcrest
+        assertThat(order.getMeals(), hasSize(1));//hamcrest
+        assertThat(order.getMeals(), not(emptyCollectionOf(Meal.class)));//hamcrest
+        assertThat(order.getMeals(), contains(meal));//hamcrest
+        assertThat(order.getMeals(), hasItem(meal));//hamcrest
+        assertThat(order.getMeals().get(0).getPrice(), equalTo(34.0));//hamcrest
+
+    }
+
+    @Test
+    public void orderMealsShouldBeInCorrectOrder() {
+        //given
+        Order order = new Order();
+        Meal pizza = new Meal("Pizza", 34);
+        Meal burger = new Meal("Burger", 25);
+
+        //when
+        order.addMealToOrder(pizza);
+        order.addMealToOrder(burger);
+
+        //then
+        assertThat(order.getMeals(), contains(pizza, burger));
+        assertThat(order.getMeals(), containsInAnyOrder(burger, pizza));
 
     }
 }
